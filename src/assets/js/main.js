f3ee1c90e06a4683db788407e20f54f4620c0e95
Ryypin <79700556+Ryypin@users.noreply.github.com>
2021-10-27 02:44:18 +0200

    ANIMATION DE MERDE V1

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,26 @@
 "use strict";
+gsap.registerPlugin(ScrollTrigger);
 
-const titleSpan = document.querySelector('.title__span');
-const color = ['#ff303b', '#44d2c1', '#4d82f6'];
-const secondColor = ['#44d275', '#98eb71', '#c327fa'];
+gsap.to(".about", {
+    scrollTrigger: {
+        trigger: ".about",
+        start: "top bottom",
+        end: "bottom bottom",
+        markers: true
+    },
+    xPercent: 100,
+    opacity: 1,
+    duration: 1
+});
 
-titleSpan.addEventListener("mouseover", (e) => {
-    console.log("Hello");
-    let randomColor = Math.floor(Math.random() * color.length);
-    let randomSecondColor = Math.floor(Math.random() * secondColor.length);
-    titleSpan.style.background = `linear-gradiant(0deg, ${randomColor} 0%, ${randomSecondColor} 100%)` 
-})
+gsap.to(".mix__block", {
+    scrollTrigger: {
+        trigger: ".section--mix",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: true,
+        pin: true
+    },
+    backgroundColor: "#c327fa",
+    duration: 1
+});
